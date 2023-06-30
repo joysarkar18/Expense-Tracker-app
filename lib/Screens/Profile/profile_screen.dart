84e551862ba263699ae3cda3c48f7b53f8539666
Joy Sarkar <joysarkar8171@gmail.com>
2023-06-30 23:28:46 +0530

    workerd on profile page ui

--- lib/Screens/Profile/profile_screen.dart
+++ lib/Screens/Profile/profile_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("pro"),
+    return Container(
+      height: Get.width,
+      child: Row(children: [
+        Container(
+          height: 300,
+          color: Colors.purple,
+        )
+      ]),
     );
   }
 }
